8ffb3bd03ff066ad3361361c0a5dc0cc2a535aff
Sanjoy Kumar Mahato <sanjoymahato.2356.csedu@gmail.com>
2026-06-21 15:20:26 +0600

    Added api gateway for event service

--- eTicket.AppHost/AppHost.cs
+++ eTicket.AppHost/AppHost.cs
@@ -1,6 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
-var eventApi = builder.AddProject<Projects.Event_API>("apiservice-event");
-var bookingApi = builder.AddProject<Projects.Booking_API>("apiservice-booking");
-var paymentApi = builder.AddProject<Projects.ePayment_API>("apiservice-payment");
+builder.AddProject<Projects.Event_API>("apiservice-event");
+builder.AddProject<Projects.Booking_API>("apiservice-booking");
+builder.AddProject<Projects.ePayment_API>("apiservice-payment");
+builder.AddProject<Projects.eTicket_ApiGateway>("eticket-apigateway");
 
 builder.Build().Run();
